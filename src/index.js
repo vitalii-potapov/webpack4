5d3c85f8b7223e35460a9a980f4116de5cc41248
Vitalii Potapov <vitalii.potapov92@gmail.com>
2019-12-17 14:50:12 +0600

    install sass

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 window.Vue = require('vue');
 
 import './js/index';
+
+import './css/index.css';
+import './scss/index.scss';
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
   entry: {
@@ -16,9 +17,40 @@ module.exports = {
         loader: 'babel-loader',
         exclude: '/node_modules/'
       },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: { sourceMap: true }
+          },
+          {
+            loader: 'postcss-loader',
+            options: { sourceMap: true, config: { path: 'src/js/config/postcss.config.js' } }
+          },
+          {
+            loader: 'sass-loader',
+            options: { sourceMap: true }
+          }
+        ]
+      },
     ]
   },
   devServer: {
     overlay: true
-  }
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css'
+    })
+  ]
 }
